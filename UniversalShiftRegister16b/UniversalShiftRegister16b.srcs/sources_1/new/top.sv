529194528001870154fb310c3e0e636722eca7b1
eburns11 <eburns11@calpoly.edu>
2025-03-06 17:42:32 -0800

    shifty

---
+++ UniversalShiftRegister16b/UniversalShiftRegister16b.srcs/sources_1/new/top.sv
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/06/2025 05:34:02 PM
+// Design Name: 
+// Module Name: top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top(
+    input Sin,
+    input [15:0] Pin,
+    output Sout,
+    output [15:0] Pout
+    );
+endmodule
